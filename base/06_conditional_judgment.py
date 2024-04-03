612a761a0d424736b08f1836ecacd4f04b248338
Rorschach-Shaw <niujian0711@163.com>
2024-04-03 16:38:09 +0800

    update

--- base/06_conditional_judgment.py
+++ base/06_conditional_judgment.py
@@ -1,22 +1,25 @@
-# if
+# 条件判断-if
+
 
 # 语法1：
 # if 条件:
 #      代码
-
 def func():
     var_a = 300
     if var_a > 200:
         print("a > 200")
     print("结束")
 
-    if var_a > 400:
-        print("a > 400")
-    else:
-        print("a <= 400 ")
 
 # 语法2：
 # if 条件:
 #      代码
 # else：
 #      代码2
+
+def func1():
+    var_a = 300
+    if var_a > 400:
+        print("a > 400")
+    else:
+        print("a <= 400 ")
